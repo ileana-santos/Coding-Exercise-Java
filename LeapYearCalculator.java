12793fa43e671aaa40db0a9aea304c9ed99b90af
ileana-santos <124452134+ileana-santos@users.noreply.github.com>
2023-02-05 15:43:56 +0100

    Update LeapYearCalculator.java

--- LeapYearCalculator.java
+++ LeapYearCalculator.java
@@ -5,10 +5,10 @@ public class LeapYearCalculator {
     public static boolean isLeapYear(int year){
 
         if ((year >= 1 && year <= 9999) && (year % 4 == 0 && year % 100 !=0 || year % 400 == 0)) {
-            //System.out.println("True");
+            System.out.println("True");
             return true;
         } else {
-            //System.out.println("False");
+            System.out.println("False");
             return false;
         }
     }
