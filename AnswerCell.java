e6c22bd30490a87703c8692b0ada069e81822319
ileana-santos <124452134+ileana-santos@users.noreply.github.com>
2023-02-20 17:40:03 +0100

    Update AnswerCell.java

--- AnswerCell.java
+++ AnswerCell.java
@@ -1,3 +1,7 @@
+/* Your cell phone rings. Return true if you should answer it. Normally you answer, 
+except in the morning you only answer if it is your mom calling. In all cases, 
+if you are asleep, you do not answer. */
+
 public class AnswerCell {
     public static void main(String[] args) {
         System.out.println(answerCell(false,false,false));
