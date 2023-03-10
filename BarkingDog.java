46377a8b5a856ab0332834dd740edf33a8558fbd
ileana-santos <124452134+ileana-santos@users.noreply.github.com>
2023-03-10 20:47:02 +0100

    Update BarkingDog.java

--- BarkingDog.java
+++ BarkingDog.java
@@ -1,6 +1,8 @@
 public class BarkingDog {
+    
     public static void main(String[] args) {
     }
+    
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if (hourOfDay < 0 || hourOfDay > 23) {
             return false;
