2614977d186e8fafa735b6daeb7c476cd87ab058
Ileana Santos <124452134+ileana-santos@users.noreply.github.com>
2024-03-08 14:37:11 +0100

    Create sum28.java

---
+++ sum28.java
@@ -0,0 +1,13 @@
+/*Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.*/
+
+public boolean sum28(int[] nums) {
+          int sum = 0;
+        for (int num : nums) {
+            if (num == 2) {
+                sum += num;
+            }
+        }
+        return sum == 8;
+}
+
+//Exercise from CodingBat
